6bd9ca1d51518ad469ae244d95986627e033355d
Toni Figueroa <toni.figueroa@gmail.com>
2015-06-03 22:55:37 +0200

    Página de creditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Quiz - Author' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
