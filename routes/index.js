1f9516137d61372543dc690d980ab1534236f05e
Toni Figueroa <toni.figueroa@gmail.com>
2015-06-16 22:33:11 +0200

    Lista de preguntas y Autoload de la DB

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.param('quizId', quizController.load);	// autoload :quizId
+
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Quiz - Author' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', 						quizController.index);
+router.get('/quizes/:quizId(\\d+)', 		quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
 
 module.exports = router;
